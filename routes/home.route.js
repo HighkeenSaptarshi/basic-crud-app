15b09fbf82afc816b830fa58993314460fa79e07
Saptarshi Singha Roy <135221302+Saptarshi-singharoy@users.noreply.github.com>
2024-10-29 13:51:38 +0530

    REST api standard added

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 // const homeController = require('../controller/home.file.controller')
-const homeController = require('../controller/home.mongo.controller')
+const homeController = require("../controller/home.mongo.controller");
 
 const router = express.Router();
 
-router.post('/add-user', homeController.addUser);
-router.get('/get-user', homeController.getUser);
-router.get('/get-user/:id', homeController.getUserById);
-router.put('/update-user/:id', homeController.updateUser);
-router.delete('/delete-user/:id', homeController.deleteUser);
+router.post("/user", homeController.addUser);
+router.get("/user", homeController.getUser);
+router.get("/user/:id", homeController.getUserById);
+router.put("/user/:id", homeController.updateUser);
+router.delete("/user/:id", homeController.deleteUser);
 
 module.exports = router;
